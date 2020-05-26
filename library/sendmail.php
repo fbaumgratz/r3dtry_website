2b5b49f263deb6e301bab40300c5e47dc121afd1
fbaumgratz <frede.nt@gmail.com>
2020-05-26 13:55:26 -0300

    index + produto + contato

---
+++ library/sendmail.php
@@ -0,0 +1,88 @@
+<?php
+  /**
+   * Sets error header and json error message response.
+   *
+   * @param  String $messsage error message of response
+   * @return void
+   */
+  function errorResponse ($messsage) {
+    header('HTTP/1.1 500 Internal Server Error');
+    die(json_encode(array('message' => $messsage)));
+  }
+
+  /**
+   * Pulls posted values for all fields in $fields_req array.
+   * If a required field does not have a value, an error response is given.
+   */
+  function constructMessageBody () {
+    $fields_req =  array("name" => true, "email" => true, "message" => true);
+    $message_body = "";
+    foreach ($fields_req as $name => $required) {
+      $postedValue = $_POST[$name];
+      if ($required && empty($postedValue)) {
+        errorResponse("$name is empty.");
+      } else {
+        $message_body .= ucfirst($name) . ":  " . $postedValue . "\n";
+      }
+    }
+    return $message_body;
+  }
+
+  header('Content-type: application/json');
+
+  //do Captcha check, make sure the submitter is not a robot:)...
+  $captcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+  $captcha_header = 'Content-type: application/x-www-form-urlencoded';
+  $captcha_post_data = http_build_query(array('secret' => '6LcD0_QUAAAAANMwjcsowX6nhgiXkAJElnUvh1of', 'response' => $_POST["g-recaptcha-response"]));//getenv('RECAPTCHA_SECRET_KEY')
+  // prefer cURL over #file_get_contents...
+  if (function_exists('curl_init')) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $captcha_url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array($captcha_header));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $captcha_post_data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = json_decode(curl_exec($ch));
+    curl_close($ch);
+  } else {
+    $opts = array('http' =>
+      array(
+        'method'  => 'POST',
+        'header'  => $captcha_header,
+        'content' => $captcha_post_data
+      )
+    );
+    $context  = stream_context_create($opts);
+    $result = json_decode(file_get_contents($captcha_url, false, $context, -1, 40000));
+  }
+
+
+  if (!$result->success) {
+    errorResponse('reCAPTCHA checked failed! Error codes: ' . join(', ', $result->{"error-codes"}));
+  }
+  //attempt to send email
+  $messageBody = constructMessageBody();
+  require './vender/php_mailer/PHPMailerAutoload.php';
+  $mail = new PHPMailer;
+  $mail->CharSet = 'UTF-8';
+  $mail->isSMTP();
+  $mail->Host = "smtp.gmail.com";
+  $mail->SMTPAuth = true;
+  $mail->Username = "todos722@gmail.com";
+  $mail->Password = "123qwe456";
+  
+  $mail->SMTPSecure = 'tls';
+  $mail->Port = 587;
+  
+  $mail->setFrom($_POST['email'], $_POST['name']);
+  $mail->addAddress("todos722@gmail.com");
+  $mail->Subject = $_POST['reason'];
+  $mail->Body  = $messageBody;
+
+  //try to send the message
+  if($mail->send()) {
+    echo json_encode(array('message' => 'Your message was successfully submitted.'));
+  } else {
+    errorResponse('An unexpected error occured while attempting to send the email: ' . $mail->ErrorInfo);
+  }
+?>
